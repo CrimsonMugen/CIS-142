e6925f2f0aeee9440828adc14c3d9ced0d7d1738
JJ <jjflorio@Outlook.com>
2025-01-29 16:21:04 -0600

    First lab 1.1 @CrimsonMugen

---
+++ Lab_1_1.py
@@ -0,0 +1,18 @@
+#lab 1.1 all information in lab is made up for educational purposes
+
+# variables
+Name1 = "John"
+Name2 = "Jake"
+Number1 = "555.555.55555"
+Number2 = "555.555.55555"
+
+# provides list format for columbs and delimiter tacs
+print("Names \t Numbers")
+print("="*22)
+
+# Alternate 1 line header
+#print("Names \t Numbers\n","="*22)
+
+# list of friends names and numbers
+print(Name1, '\t', Number1)
+print(Name2, '\t', Number2)
