f3ec675da00c05f2671729950f71490f70493384
JJ <jjflorio@Outlook.com>
2025-02-20 06:04:55 -0600

    lab 2.2 @CrimsonMugen

---
+++ Lab_2_2.py
@@ -0,0 +1,11 @@
+##
+# @author Johnathan Florio
+# lab 2.2 perform simple arithmatic
+
+firstInt = int(input("Enter first positive integer: "))
+secondInt = int(input("Enter second positive intiger: "))
+print()
+print(f'Sum: {firstInt + secondInt}')
+print(f'Product: {firstInt * secondInt}')
+print(f'Diffrence: {firstInt - secondInt}')
+print(f'Quotient: {firstInt / secondInt}')
